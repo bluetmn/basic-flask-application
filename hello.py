fedcdaae2566edf23f56a1dcd766db1d8adcd1e2
Thomas Ryan <thomasryan2118@gmail.com>
2021-09-28 20:58:27 -0500

    basic modifications using jinja

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 #hello.py
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html' , pageTitle='Flask Server Home Page')
 
 @app.route('/winner')
 def index():
-    return 'You win!!'
+    return render_template('winner.html', pageTitle='Winner Page')
+
+if __name__ == '__main__':
+    app.run(debug=True)
